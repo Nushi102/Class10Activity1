11b8a22c17fe5363460b8b0821886bddac892b13
Nushi102 <88207136+Nushi102@users.noreply.github.com>
2021-08-02 10:16:21 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var weights= [35,38,40,45]
+
+
+
+function setup() 
+{
+  createCanvas(400,400);
+ sum = weights[0]+weights[1]+weights[2]+weights[3]
+ console.log(sum)
+ avg = sum/weights.length
+ console.log(avg)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
